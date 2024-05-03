2130447654a2af2c0b8c3486819ae0f05f4dc926
Prathamesh Karatkar <117072460+prathameshkaratkar@users.noreply.github.com>
2024-05-03 19:59:29 +0530

    Create orders.js

---
+++ orders.js
@@ -0,0 +1,78 @@
+[
+  {
+    "_id": "624c9ebe6c7d9c4d9c8b456a",
+    "user": "624c9a6e6c7d9c4d9c8b4567",
+    "products": [
+      {
+        "product": "624c9b3a6c7d9c4d9c8b4568",
+        "quantity": 2
+      },
+      {
+        "product": "624c9b5e6c7d9c4d9c8b4569",
+        "quantity": 1
+      }
+    ],
+    "totalAmount": 59.98
+  },
+  {
+    "_id": "624c9ebe6c7d9c4d9c8b456b",
+    "user": "624c9a6e6c7d9c4d9c8b4568",
+    "products": [
+      {
+        "product": "624c9b7a6c7d9c4d9c8b456a",
+        "quantity": 3
+      }
+    ],
+    "totalAmount": 44.97
+  },
+  {
+    "_id": "624c9ebe6c7d9c4d9c8b456c",
+    "user": "624c9a6e6c7d9c4d9c8b4569",
+    "products": [
+      {
+        "product": "624c9b8e6c7d9c4d9c8b456b",
+        "quantity": 1
+      },
+      {
+        "product": "624c9ba26c7d9c4d9c8b456c",
+        "quantity": 1
+      }
+    ],
+    "totalAmount": 64.98
+  },
+  {
+    "_id": "624c9ebe6c7d9c4d9c8b456d",
+    "user": "624c9a6e6c7d9c4d9c8b456a",
+    "products": [
+      {
+        "product": "624c9bb66c7d9c4d9c8b456d",
+        "quantity": 2
+      },
+      {
+        "product": "624c9bca6c7d9c4d9c8b456e",
+        "quantity": 1
+      }
+    ],
+    "totalAmount": 32.97
+  },
+  {
+    "_id": "624c9ebe6c7d9c4d9c8b456e",
+    "user": "624c9a6e6c7d9c4d9c8b456b",
+    "products": [
+      {
+        "product": "624c9bde6c7d9c4d9c8b456f",
+        "quantity": 3
+      }
+    ],
+    "totalAmount": 53.97
+  },
+  {
+    "_id": "624c9ebe6c7d9c4d9c8b456f",
+    "user": "624c9a6e6c7d9c4d9c8b456c",
+    "products": [
+      {
+        "product": "624c9bf26c7d9c4d9c8b4570",
+        "quantity": 2
+      }
+    ],
+    "totalAmount":
