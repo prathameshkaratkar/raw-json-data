20acf0714a53441b7129f4bc59022ec70a3484dc
Prathamesh Karatkar <117072460+prathameshkaratkar@users.noreply.github.com>
2024-05-03 19:59:52 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,62 @@
+[
+  {
+    "_id": "624c9b3a6c7d9c4d9c8b4568",
+    "name": "Product 1",
+    "description": "This is the first product.",
+    "price": 19.99
+  },
+  {
+    "_id": "624c9b5e6c7d9c4d9c8b4569",
+    "name": "Product 2",
+    "description": "This is the second product.",
+    "price": 29.99
+  },
+  {
+    "_id": "624c9b7a6c7d9c4d9c8b456a",
+    "name": "Product 3",
+    "description": "This is the third product.",
+    "price": 14.99
+  },
+  {
+    "_id": "624c9b8e6c7d9c4d9c8b456b",
+    "name": "Product 4",
+    "description": "This is the fourth product.",
+    "price": 24.99
+  },
+  {
+    "_id": "624c9ba26c7d9c4d9c8b456c",
+    "name": "Product 5",
+    "description": "This is the fifth product.",
+    "price": 39.99
+  },
+  {
+    "_id": "624c9bb66c7d9c4d9c8b456d",
+    "name": "Product 6",
+    "description": "This is the sixth product.",
+    "price": 9.99
+  },
+  {
+    "_id": "624c9bca6c7d9c4d9c8b456e",
+    "name": "Product 7",
+    "description": "This is the seventh product.",
+    "price": 12.99
+  },
+  {
+    "_id": "624c9bde6c7d9c4d9c8b456f",
+    "name": "Product 8",
+    "description": "This is the eighth product.",
+    "price": 17.99
+  },
+  {
+    "_id": "624c9bf26c7d9c4d9c8b4570",
+    "name": "Product 9",
+    "description": "This is the ninth product.",
+    "price": 22.99
+  },
+  {
+    "_id": "624c9c066c7d9c4d9c8b4571",
+    "name": "Product 10",
+    "description": "This is the tenth product.",
+    "price": 32.99
+  }
+]
