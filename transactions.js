72b6e2e7228755301c466d8aa99213b6b19ac9bd
Prathamesh Karatkar <117072460+prathameshkaratkar@users.noreply.github.com>
2024-05-03 20:00:20 +0530

    Create transactions.js

---
+++ transactions.js
@@ -0,0 +1,62 @@
+[
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b4571",
+    "order": "624c9ebe6c7d9c4d9c8b456a",
+    "paymentMethod": "Credit Card",
+    "transactionAmount": 59.98
+  },
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b4572",
+    "order": "624c9ebe6c7d9c4d9c8b456b",
+    "paymentMethod": "PayPal",
+    "transactionAmount": 44.97
+  },
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b4573",
+    "order": "624c9ebe6c7d9c4d9c8b456c",
+    "paymentMethod": "Debit Card",
+    "transactionAmount": 64.98
+  },
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b4574",
+    "order": "624c9ebe6c7d9c4d9c8b456d",
+    "paymentMethod": "Credit Card",
+    "transactionAmount": 32.97
+  },
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b4575",
+    "order": "624c9ebe6c7d9c4d9c8b456e",
+    "paymentMethod": "PayPal",
+    "transactionAmount": 53.97
+  },
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b4576",
+    "order": "624c9ebe6c7d9c4d9c8b456f",
+    "paymentMethod": "Credit Card",
+    "transactionAmount": 45.98
+  },
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b4577",
+    "order": "624c9ebe6c7d9c4d9c8b4570",
+    "paymentMethod": "Debit Card",
+    "transactionAmount": 79.97
+  },
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b4578",
+    "order": "624c9ebe6c7d9c4d9c8b4571",
+    "paymentMethod": "PayPal",
+    "transactionAmount": 99.99
+  },
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b4579",
+    "order": "624c9ebe6c7d9c4d9c8b4572",
+    "paymentMethod": "Credit Card",
+    "transactionAmount": 24.99
+  },
+  {
+    "_id": "624c9f5a6c7d9c4d9c8b457a",
+    "order": "624c9ebe6c7d9c4d9c8b4573",
+    "paymentMethod": "Debit Card",
+    "transactionAmount": 54.98
+  }
+]
